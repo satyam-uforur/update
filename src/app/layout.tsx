e9e6cb4a33b6099cc94e7cf3b1b3f97b07007fbd
Apple <apple@192.168.1.6>
2025-08-28 11:07:25 +0530

    Dashboard Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession(authOptions);
 
   return (
     <html lang="en">
@@ -62,7 +61,7 @@ export default async function RootLayout({
           zIndex={1600}
           showAtBottom={false}
         />
-        <AuthProvider session={session}>
+        <AuthProvider>
           <UserProvider>
             <Toaster />
             {children}
--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,16 +1,12 @@
+// components/AuthProvider.tsx
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
 
-const AuthProvider = ({
+export default function AuthProvider({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: Session | null;
-}) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
-};
-
-export default AuthProvider;
+}) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
