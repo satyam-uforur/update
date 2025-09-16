6201ab0afb44b072f439774f30beee9d1a742d97
Satyam Tiwari <159171580+satyam-uforur@users.noreply.github.com>
2025-09-16 20:18:52 +0530

    Update fingerprint-scanner.tsx

--- src/components/fingerprint-scanner.tsx
+++ src/components/fingerprint-scanner.tsx
@@ -55,7 +55,7 @@ export default function FingerprintScanner({ className = "" }: FingerprintScanne
             setScannedFingers([])
             setCurrentFinger(0)
             isProcessing.current = false // Reset debounce
-          }, 5000)
+          }, 2000)
 
           return 100
         }
